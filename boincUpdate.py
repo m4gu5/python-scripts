a409157497ed3ac873fabb26900e9511b9bbea0a
m4gu5 <m4gu5@gmx.de>
2015-11-07 19:52:20 +0100

    * added interval validation * will now print errors * password is now required

--- boincUpdate.py
+++ boincUpdate.py
@@ -10,11 +10,18 @@ import argparse
 import subprocess
 import time
 
+def query_interval(x):
+    # Prevent flooding
+    x = int(x)
+    if x < 120:
+        raise argparse.ArgumentTypeError("Interval must be at least 120")
+    return x
+
 parser = argparse.ArgumentParser(description='Periodically update a BOINC project')
 parser.add_argument('--host', default='127.0.0.1', help='The host to contact (default: localhost)', type=str)
-parser.add_argument('--passwd', default='""', help='The password of the BOINC instance (default: empty)', type=str)
+parser.add_argument('--passwd', help='The password of the BOINC instance (default: empty)', type=str, required=True)
 parser.add_argument('--project', help='The URL of the project which should be queried', type=str, required=True)
-parser.add_argument('--interval', default=120, help='Number of seconds to wait between each update request (default: 120)', type=int)
+parser.add_argument('--interval', default=120, help='Number of seconds to wait between each update request (default: 120)', type=query_interval)
 args = parser.parse_args()
 
 host = args.host
@@ -28,6 +35,9 @@ while query:
         print('[' + time.strftime('%Y-%m-%d %H:%M:%S') + '] Updating ' + project + ' on host ' + host)
         p = subprocess.Popen(['boinccmd', '--host', host, '--passwd', passwd, '--project', project, 'update'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
+        if not err.rstrip('\n'):
+            # stderr output is not empty
+            print(err)
         time.sleep(interval)
     except KeyboardInterrupt:
         query = False
